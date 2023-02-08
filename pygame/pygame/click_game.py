c0778e573ae668990e7dea99b70427b6953d31c7
amkeberle <113544611+amkeberle@users.noreply.github.com>
2023-02-08 13:59:32 -0500

    Add files via upload

---
+++ pygame/pygame/click_game.py
@@ -0,0 +1,23 @@
+blob= Actor('character1')
+blob.position = 200, 200
+
+BACKGROUND_COLOR = (0,0,255)
+WIDTH = 500
+HEIGHT = 300
+
+def draw():
+    screen.fill((0,0,255))
+    blob.draw()
+def update():
+    blob.left = blob.left + 2
+    if blob.left > WIDTH:
+        blob.right = 0
+def on_mouse_down(pos):
+    if blob.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("You missed me!")
+def on_mouse_down(pos):
+    if blob.collidepoint(pos):
+        sounds.eep.play()
+        blob.image = 'character2'
